9faf803808f9d8de2a0aa2c5aaaa1a977fe0cc97
changhee <oche.edu@gmail.com>
2025-03-24 10:58:34 +0900

    Create 기능 구현

---
+++ articles/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    # Create
+    path('create/', views.create, name='create'),
+    # Read
+
+    # Update
+
+    # Delete
+]
